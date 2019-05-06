ae6bda3309dc64bca431642de552bff7bb3db14f
tedu <tedu@DESKTOP-67KFJT0>
2019-05-06 09:42:08 +0800

    Test3

--- dadas/src/main/java/dah/Test1.java
+++ dadas/src/main/java/dah/Test1.java
@@ -1,8 +1,8 @@
 package dah;
 
 public class Test1 {
-  
-  public static void main(String[] args){
-    System.out.println(8);
-  }
+
+	public static void main(String[] args){
+		System.out.println(8);
+	}
 }
